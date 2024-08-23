2cc0839764e899c6bc06c69a1b7463c28af1a221
Reinmmar <31139601+TimeStepYT@users.noreply.github.com>
2024-08-23 16:42:14 +0200

    split class into files

---
+++ src/CCKeyboardDispatcher.cpp
@@ -0,0 +1,15 @@
+#include "include.h"
+
+class $modify(MyHookLol, CCKeyboardDispatcher) {
+	// Needed because BetterInfo has special FLAlertLayers that duplicate for some reason
+	bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool idk) {
+		if (blockKeys && down && Loader::get()->isModLoaded("cvolton.betterinfo")) {
+			if (key == enumKeyCodes::KEY_Left || key == enumKeyCodes::KEY_Right)
+				return true;
+			else if (key == enumKeyCodes::KEY_Escape)
+				blockKeys = false;
+		}
+
+		return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, idk);
+	}
+};
---
+++ src/include.h
@@ -0,0 +1,8 @@
+#include <Geode/Geode.hpp>
+
+#include <Geode/modify/FLAlertLayer.hpp>
+#include <Geode/modify/CCKeyboardDispatcher.hpp>
+
+using namespace geode::prelude;
+
+extern bool blockKeys;
