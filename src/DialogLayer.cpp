07a42249aa9b01bcb9cf329364c8601732393e06
Reinmmar <31139601+TimeStepYT@users.noreply.github.com>
2024-08-27 21:47:45 +0200

    queueInMainThread instead of schedule for delay

--- src/DialogLayer.cpp
+++ src/DialogLayer.cpp
@@ -7,27 +7,23 @@ bool DeltaruneDialogLayer::init(DialogObject* p0, CCArray* p1, int p2) {
 	char const* title = m_fields->p0->m_character.c_str();
 	gd::string text = m_fields->p0->m_text;
 	auto alert = FLAlertLayer::create(title, text, "OK");
-
 	auto texture = m_characterSprite->getTexture();
-#ifdef GEODE_IS_ANDROID || GEODE_IS_MACOS
-	static_cast<DeltaruneAlertLayer*>(alert)->m_fields->characterSprite = CCSpriteGrayscale::createWithTexture(texture);
-#else
+	
+#ifdef GEODE_IS_WINDOWS
 	static_cast<DeltaruneAlertLayer*>(alert)->m_fields->characterSprite = CCSprite::createWithTexture(texture);
+#else
+	static_cast<DeltaruneAlertLayer*>(alert)->m_fields->characterSprite = CCSpriteGrayscale::createWithTexture(texture);
 #endif
 	static_cast<DeltaruneAlertLayer*>(alert)->m_fields->dialog = true;
 	alert->show();
 
 	this->setOpacity(0);
-	this->schedule(schedule_selector(DeltaruneDialogLayer::closeWithDelay));
+	geode::Loader::get()->queueInMainThread([this] {
+		onClose();
+		});
 	return true;
 }
 
 void DeltaruneDialogLayer::displayDialogObject(DialogObject* p0) {
 	DialogLayer::displayDialogObject(p0);
-}
-
-void DeltaruneDialogLayer::closeWithDelay(float dt) {
-	if (!m_characterSprite) return;
-	onClose();
-	unschedule(schedule_selector(DeltaruneDialogLayer::closeWithDelay));
 }
