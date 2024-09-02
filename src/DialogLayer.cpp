b6aad880d648779bf5f761e0bca8400c321a4732
Reinmmar <31139601+TimeStepYT@users.noreply.github.com>
2024-09-02 21:46:47 +0200

    cleanup and maybe fix???

--- src/DialogLayer.cpp
+++ src/DialogLayer.cpp
@@ -6,11 +6,12 @@ bool DeltaruneDialogLayer::init(DialogObject* p0, CCArray* p1, int p2) {
 	m_fields->p0 = p0;
 	char const* title = m_fields->p0->m_character.c_str();
 	std::string text = m_fields->p0->m_text;
-	auto alert = FLAlertLayer::create(title, text, "OK");
+	auto unmodifiedAlert = FLAlertLayer::create(title, text, "OK");
 	auto texture = m_characterSprite->getTexture();
 
-	static_cast<DeltaruneAlertLayer*>(alert)->m_fields->characterSprite = CCSpriteGrayscale::createWithTexture(texture);
-	static_cast<DeltaruneAlertLayer*>(alert)->m_fields->dialog = true;
+	auto alert = static_cast<DeltaruneAlertLayer*>(unmodifiedAlert);
+	alert->m_fields->characterSprite = CCSpriteGrayscale::createWithTexture(texture);
+	alert->m_fields->dialog = true;
 	alert->show();
 
 	this->setOpacity(0);
