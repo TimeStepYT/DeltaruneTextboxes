260b594566a936882f8b76e95c1c667bad1b3c80
Reinmmar <herwig.orlich@gmail.com>
2024-09-12 18:07:11 +0200

    sorry mac

--- src/include.h
+++ src/include.h
@@ -1,8 +1,9 @@
 #include <Geode/Geode.hpp>
 
 #include <Geode/fmod/fmod.hpp>
+#if !defined(GEODE_IS_MACOS) || !defined(DEBUG_MAC_INPUTS)
 #include <geode.custom-keybinds/include/Keybinds.hpp>
-
+#endif
 #include <Geode/modify/FLAlertLayer.hpp>
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 #include <Geode/modify/DialogLayer.hpp>
