2f61b3eb3abf9d49931e4c021999b3223bdb9577
TimeStepYT <31139601+TimeStepYT@users.noreply.github.com>
2026-07-05 00:51:25 +0200

    Imagine if I left this in the release lmao

--- src/hooks/CCKeyboardDispatcher.cpp
+++ src/hooks/CCKeyboardDispatcher.cpp
@@ -3,7 +3,7 @@
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 #include <Geode/ui/Button.hpp>
 
-#include "../Events.hpp"
+// #include "../Events.hpp"
 
 using namespace geode::prelude;
 
@@ -23,17 +23,17 @@ class $modify(MyHookLol, CCKeyboardDispatcher) {
 		// }
 
 		
-		if (key == KEY_U) {
-			auto characterSprite1 = CircleButtonSprite::create(nullptr, CircleBaseColor::Blue, CircleBaseSize::Large);
-			auto characterSprite2 = CircleButtonSprite::create(nullptr, CircleBaseColor::Cyan, CircleBaseSize::Large);
-			std::vector<DeltaruneDialogObject> objects;
-			objects.emplace_back(characterSprite1, "Queen", "Queen", "Poor Noelle She Would Love Hitting All These Cars");
-			objects.emplace_back(characterSprite2, "Noelle", "Noelle", "Umm, actually, I... she, um, might not, um...");
-			objects.emplace_back(characterSprite1, "Queen", "Queen", "Stop Everything");
-			objects.emplace_back(characterSprite1, "Queen", "Queen", "Kris Get The Banana");
-			objects.emplace_back(characterSprite1, "Queen", "Queen", "Potassium");
-			CreateDialogFull().send(nullptr, objects);
-		}
+		// if (key == KEY_U) {
+		// 	auto characterSprite1 = CircleButtonSprite::create(nullptr, CircleBaseColor::Blue, CircleBaseSize::Large);
+		// 	auto characterSprite2 = CircleButtonSprite::create(nullptr, CircleBaseColor::Cyan, CircleBaseSize::Large);
+		// 	std::vector<DeltaruneDialogObject> objects;
+		// 	objects.emplace_back(characterSprite1, "Queen", "Queen", "Poor Noelle She Would Love Hitting All These Cars");
+		// 	objects.emplace_back(characterSprite2, "Noelle", "Noelle", "Umm, actually, I... she, um, might not, um...");
+		// 	objects.emplace_back(characterSprite1, "Queen", "Queen", "Stop Everything");
+		// 	objects.emplace_back(characterSprite1, "Queen", "Queen", "Kris Get The Banana");
+		// 	objects.emplace_back(characterSprite1, "Queen", "Queen", "Potassium");
+		// 	CreateDialogFull().send(nullptr, objects);
+		// }
 
 		if (global::blockKeys && Loader::get()->isModLoaded("cvolton.betterinfo")) {
 			if (key == KEY_Left || key == KEY_Right)
